4e1b9652db50abba6f49d926c095e7bf1eaaa971
j314h <johnhaimez314@gmail.com>
2021-12-26 17:40:44 +0100

    ajout de couleur + font dans tailwind

--- sveltekit/tailwind.config.cjs
+++ sveltekit/tailwind.config.cjs
@@ -2,7 +2,19 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		colors: {
+			fondPrincipalClaire: '#ffffff',
+			fondPrincipalDark: '#331133',
+			color1: '#0057FF',
+			color2: '#CC00FF',
+			color3: '#FFB800',
+			color4: '#19B500'
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Roboto', 'sans-serif']
+			}
+		}
 	},
 
 	plugins: []
