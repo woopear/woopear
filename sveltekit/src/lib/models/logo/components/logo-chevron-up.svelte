3afc1dbf51f5fb5df9956ac4cc970aaaa2e7870b
j314h <johnhaimez314@gmail.com>
2021-12-27 17:26:20 +0100

    fixes #6 ; ajout logo chevronUp + ajout btnReturnUp

---
+++ sveltekit/src/lib/models/logo/components/logo-chevron-up.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="h-6 w-6 md:h-8 md:w-8 lg:h-10 lg:w-10"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke="currentColor"
+>
+	<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+</svg>
+
+<!--
+  @component
+
+  ## logo-chevron-up
+
+  > logo qui représente un chevron touné vers le haut  
+
+  ### props
+
+  - RAS
+
+  ### exemple d'utilisation
+
+  ```js
+  <LogoChevronUp />
+  ```
+
+  ### fonctions / variables
+
+  - RAS
+
+  ### les imports
+
+  - RAS
+
+-->
--- sveltekit/src/lib/models/menu-float/components/menu-float.svelte
+++ sveltekit/src/lib/models/menu-float/components/menu-float.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+	import BtnReturnUp from '$lib/models/btn-return-up/components/btn-return-up.svelte';
 	import LogoConnexion from '$lib/models/logo/components/logo-connexion.svelte';
 	import LogoDarkMode from '$lib/models/logo/components/logo-dark-mode.svelte';
 </script>
 
-<nav class="fixed right-6 bottom-6 z-40">
+<nav class="fixed right-6 bottom-12 z-40">
 	<ul>
 		<LogoConnexion addStyleDiv="mb-4" />
 		<LogoDarkMode addStyleDiv="mb-4" />
+		<BtnReturnUp addStyleDiv="mb-4" />
 	</ul>
 </nav>
 
