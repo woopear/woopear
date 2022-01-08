9bb2af369d68d2f9d0c25c8669ccea4158062398
j314h <johnhaimez314@gmail.com>
2022-01-08 16:11:22 +0100

    creation composant titlePartRubric

---
+++ sveltekit/src/lib/models/title-part-rubric/components/title-part-rubric.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { filterStringService } from '$lib/providers/filter-string/filter-string.service';
+
+	export let text: string;
+</script>
+
+<h4 class="font-bold">{filterStringService.firstToUppperCase(text)}</h4>
