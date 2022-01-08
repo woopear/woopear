0519d454158e67fbceeb2cdfd9490d0b8310ce09
j314h <johnhaimez314@gmail.com>
2022-01-08 11:29:37 +0100

    ajout menu pour dashboard

--- sveltekit/src/lib/models/header/components/header.svelte
+++ sveltekit/src/lib/models/header/components/header.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import LogoWoopear from '$lib/models/logo/components/logo-woopear.svelte';
 	import NavItem from './nav-item.svelte';
 	import { constItemMenuHeader } from '../stores/header.const';
-	import { page } from '$app/stores';
+	import HeaderMenu from './header-menu.svelte';
 
 	let styleDiv = `group close cursor-pointer h-12 w-12 relative ml-2`;
 	let addStyleNav = `-left-full`;
@@ -22,65 +21,56 @@
 <header
 	class="transition-all duration-300 shadow-md flex items-center flex-none fixed bg-fondPrincipalClaire pl-2 pr-4 h-24 w-full border-b border-gray-300 md:pl-4 dark:bg-fondPrincipalDark z-30"
 >
-	<!-- menu sandwich -->
-	{#if $page.path === '/'}
-		<div class="flex justify-between w-full items-center md:hidden">
-			<!-- barre du menu -->
-			<div on:click={clickForSeeMenu} class={styleDiv}>
-				<div
-					class="bg-fondPrincipalDark absolute rounded-full w-8 h-1 top-4 left-2 dark:bg-fondPrincipalClaire"
-				/>
-				<div
-					class="bg-fondPrincipalDark absolute rounded-full w-8 h-1 top-7 left-2 dark:bg-fondPrincipalClaire"
-				/>
-			</div>
-		</div>
-	{/if}
-
-	<!-- logo woopear -->
-	<LogoWoopear size="w-10 h-10" />
-
-	<!-- contenu du menu -->
-	{#if $page.path === '/'}
-		<nav
-			class={`transition-all duration-300 absolute top-24 ${addStyleNav} w-full min-h-screen md:static md:min-h-min`}
+	<HeaderMenu handlerClickMenuSandwich={clickForSeeMenu} {styleDiv} {addStyleNav}>
+		<!-- menu principal -->
+		<div
+			slot="menu-principal"
+			class="px-4 py-4 bg-fondPrincipalClaire min-h-screen md:min-h-min md:bg-transparent md:flex md:flex-row md:p-0 dark:bg-fondPrincipalDark"
 		>
-			<ul
-				class="px-4 py-4 bg-fondPrincipalClaire min-h-screen md:min-h-min md:bg-transparent md:flex md:flex-row md:p-0 dark:bg-fondPrincipalDark"
-			>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.home.libelle}
-					link={constItemMenuHeader.home.path}
-				/>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.method.libelle}
-					link={constItemMenuHeader.method.path}
-				/>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.service.libelle}
-					link={constItemMenuHeader.service.path}
-				/>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.team.libelle}
-					link={constItemMenuHeader.team.path}
-				/>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.product.libelle}
-					link={constItemMenuHeader.product.path}
-				/>
-				<NavItem
-					handlerClickA={clickForSeeMenu}
-					libelle={constItemMenuHeader.contact.libelle}
-					link={constItemMenuHeader.contact.path}
-				/>
-			</ul>
-		</nav>
-	{/if}
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.home.libelle}
+				link={constItemMenuHeader.home.path}
+			/>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.method.libelle}
+				link={constItemMenuHeader.method.path}
+			/>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.service.libelle}
+				link={constItemMenuHeader.service.path}
+			/>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.team.libelle}
+				link={constItemMenuHeader.team.path}
+			/>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.product.libelle}
+				link={constItemMenuHeader.product.path}
+			/>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.contact.libelle}
+				link={constItemMenuHeader.contact.path}
+			/>
+		</div>
+
+		<!-- menu dashboard -->
+		<div
+			slot="menu-dashboard"
+			class="px-4 py-4 bg-fondPrincipalClaire min-h-screen md:min-h-min md:bg-transparent md:flex md:flex-row md:p-0 dark:bg-fondPrincipalDark"
+		>
+			<NavItem
+				handlerClickA={clickForSeeMenu}
+				libelle={constItemMenuHeader.contact.libelle}
+				link={constItemMenuHeader.contact.path}
+			/>
+		</div>
+	</HeaderMenu>
 </header>
 
 <!--
@@ -118,29 +108,3 @@
 	```
 
 -->
-<style>
-	.open {
-		transition: all 0.6s;
-	}
-	.open :nth-child(1) {
-		top: 22px;
-		left: 8px;
-		transform: rotate(315deg);
-		transition: all 0.6s;
-	}
-	.open :nth-child(2) {
-		top: 22px;
-		left: 8px;
-		transform: rotate(-315deg);
-		transition: all 0.6s;
-	}
-	.close {
-		transition: all 0.6s;
-	}
-	.close :nth-child(1) {
-		transition: all 0.6s;
-	}
-	.close :nth-child(2) {
-		transition: all 0.6s;
-	}
-</style>
