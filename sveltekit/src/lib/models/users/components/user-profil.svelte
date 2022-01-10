049ac04e85cdebd289dc178f4cada4d49abe8714
j314h <johnhaimez314@gmail.com>
2022-01-10 07:52:13 +0100

    creation btn modification profil

--- sveltekit/src/lib/models/users/components/user-profil.svelte
+++ sveltekit/src/lib/models/users/components/user-profil.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import BoxPartRubric from '$lib/models/box-part-rubric/components/box-part-rubric.svelte';
 	import BoxRubric from '$lib/models/box-rubric/components/box-rubric.svelte';
+	import LogoPencil from '$lib/models/logo/components/logo-pencil.svelte';
 	import TitlePartRubric from '$lib/models/title-part-rubric/components/title-part-rubric.svelte';
 	import { filterStringService } from '$lib/providers/filter-string/filter-string.service';
 	import { userStore } from '../stores/user.store';
@@ -9,8 +10,9 @@
 {#if $userStore.userCurrent.id}
 	<BoxRubric addStyle="w-72" addStyleDiv="p-4">
 		<!-- title box -->
-		<div>
+		<div class="flex justify-between items-center">
 			<h1 class="font-bold text-lg">Mon profil</h1>
+			<LogoPencil handlerClick={undefined} />
 		</div>
 
 		<!-- partie affichage -->
