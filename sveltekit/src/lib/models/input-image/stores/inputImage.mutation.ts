60a56aa893c7728649c6a6698d682e0d9ff9a89f
David-Caignaert <davidcaignaert@gmail.com>
2022-01-14 10:42:23 +0100

    feat #35,ajout de la fonction removeImage dans le fichier mutation

--- sveltekit/src/lib/models/input-image/stores/inputImage.mutation.ts
+++ sveltekit/src/lib/models/input-image/stores/inputImage.mutation.ts
@@ -13,11 +13,23 @@ export const inputImageMutation = {
     });
   },
 
+  /**
+   * ajouter une image
+   * @param value objet de l'image a ajouter
+   */
   addImage: (value: IImage): void => {
     inputImageStore.update((i) => {
       // si la mention existe deja on l'enleve
       i.inputImages = i.inputImages.filter((el) => el !== value);
       i.inputImages = [...i.inputImages, value];
+      return i;
+    })
+  },
+
+  removeImage: (id: string): void => {
+    inputImageStore.update((i) => {
+      // si la mention existe deja on l'enleve
+      i.inputImages = i.inputImages.filter((el) => el.id !== id);
       return i;
     })
   }
