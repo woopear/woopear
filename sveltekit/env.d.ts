b8053276c67d9396382313dc0f830c5b3d07e2ac
j314h <johnhaimez314@gmail.com>
2022-01-15 08:01:18 +0100

    ras

--- sveltekit/env.d.ts
+++ sveltekit/env.d.ts
@@ -5,8 +5,6 @@ interface ImportMetaEnv {
 	readonly VITE_GRAPHQL_STRAPI_BACK: string;
 	readonly VITE_URL_API_IMG: string;
 	readonly VITE_URL_API: string;
-	readonly VITE_GRAPHQL_FAUNA: string;
-	readonly VITE_SECRET_PUBLIC_FAUNA: string;
 }
 
 interface ImportMeta {
--- sveltekit/src/lib/models/graphql/graphql.service.ts
+++ sveltekit/src/lib/models/graphql/graphql.service.ts
@@ -2,18 +2,6 @@ import { GraphQLClient } from 'graphql-request';
 
 const graphqlUrl = import.meta.env.VITE_GRAPHQL_STRAPI_FRONT;
 const graphqlUrlBack = import.meta.env.VITE_GRAPHQL_STRAPI_BACK;
-const graphqlUrlFauna = import.meta.env.VITE_GRAPHQL_FAUNA;
 
 export const graphqlService = new GraphQLClient(graphqlUrl);
 export const graphqlServiceBack = new GraphQLClient(graphqlUrlBack);
-
-export const graphQLClient = (token: string): GraphQLClient => {
-	const secret = token || import.meta.env.VITE_SECRET_PUBLIC_FAUNA;
-
-	return new GraphQLClient(graphqlUrlFauna, {
-		headers: {
-			authorization: `Bearer ${secret}`
-			// 'X-Schema-Preview': 'partial-update-mutation', // move to `pages/index.js`
-		}
-	});
-};
