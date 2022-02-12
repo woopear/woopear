57fa5fef7c4cdb8e1596a1333fc8766a276eb8b6
j314h <johnhaimez314@gmail.com>
2022-02-12 21:17:18 +0100

    ras

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/error.svelte.js';
-
-export { module };
-export const entry = 'error.svelte-60b5f229.js';
-export const js = ["error.svelte-60b5f229.js","chunks/vendor-f0095a1c.js"];
-export const css = [];
