063835475ab964ddf4715c48e69b86819d83e03c
David-Caignaert <davidcaignaert@gmail.com>
2022-02-15 16:42:05 +0100

    feat #112, interface team woopear et commentaire interface article

--- src/lib/modules/article/article.type.ts
+++ src/lib/modules/article/article.type.ts
@@ -1,24 +1,36 @@
-export interface IArticle {
-  id?: string,
-  title?: string,
-  image?: string,
-  content?: IArticleContent
-}
-
+/* 
+* interface content de l'article
+*/
 export interface IArticleContent {
   id?: string,
   sub_title: string,
   text?: string
 }
 
+/* 
+* interface article
+*/
+export interface IArticle {
+  id?: string,
+  title?: string,
+  image?: string,
+  content?: IArticleContent[]
+}
+
+/* 
+* interface label de l'adverting
+*/
+export interface IAdvertingLabel {
+  libelle?: string,
+  description?:string
+}
+
+/* 
+* interface de l'adverting
+*/
 export interface IAdverting {
   id?: string,
   title?: string,
   imaage?: string,
   label?: IAdvertingLabel
-}
-
-export interface IAdvertingLabel {
-  libelle?: string,
-  description?:string
 }
