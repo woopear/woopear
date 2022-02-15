a16f3d8ba5c24a5e2e2876b5c822014f06f1ba0c
David-Caignaert <davidcaignaert@gmail.com>
2022-02-15 17:14:50 +0100

    feat #112, interface condition et user

---
+++ src/lib/modules/user/user.type.ts
@@ -0,0 +1,37 @@
+/* 
+* interface companie du user
+*/
+export interface IUserCompanie {
+  denomination?: string,
+  siret?: string,
+  code_naf?: string,
+  address?: string,
+  code_post?: string,
+  city?: string,
+  logo?: string
+}
+
+/* 
+* interface role du user
+*/
+export interface IUserRole {
+  libelle?: string,
+  description?: string
+}
+
+/* 
+* interface user
+*/
+export interface IUser {
+  id?: string,
+  first_name?: string,
+  last_name?: string,
+  username?: string,
+  address?: string,
+  code_post?: string,
+  city?: string,
+  phone_number?: string,
+  avatar?: string,
+  compani?: IUserCompanie,
+  role?: IUserRole,
+}
