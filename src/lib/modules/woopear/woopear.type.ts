8e14c53d63e58e14bb46c0284e0eba1374b674a8
j314h <johnhaimez314@gmail.com>
2022-02-15 16:32:25 +0100

    creation type footer, menu, link

---
+++ src/lib/modules/woopear/woopear.type.ts
@@ -0,0 +1,12 @@
+// interface de l'objet woopear
+export interface IWoopear {
+  id?: string;
+  name?: string;
+  email?: string;
+  phone_number?: string;
+  address?: string;
+  code_post?: string;
+  city?: string;
+  info?: string;
+  map?: string;
+}
