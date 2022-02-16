8de8d10c57ee70a17fdb7c19a7a0cbff6aafce25
David-Caignaert <davidcaignaert@gmail.com>
2022-02-16 17:20:40 +0100

    fixes #107;ajout d un dossier component dans header et footer,ajout de balise section-ul-li

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <script>
   import '../app.css';
   import Main from '$lib/modules/main/components/main.svelte';
-  import HeaderHome from '$lib/modules/header/header-home.component.svelte';
-  import Footer from '$lib/modules/footer/footer.component.svelte';
+  import HeaderHome from '$lib/modules/header/component/header-home.component.svelte';
+  import Footer from '$lib/modules/footer/component/footer.component.svelte';
 </script>
 
 <!-- header -->
