04045736d788e77762870347323f368a5f10a6f2
David-Caignaert <davidcaignaert@gmail.com>
2022-02-16 11:48:31 +0100

    fixe #108, fixe #115 ;mise en place du header-home et du header-account + mise en place du mode dark

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,13 @@
 <script>
   import '../app.css';
   import Main from '$lib/modules/main/components/main.svelte';
+  import HeaderHome from '$lib/modules/header/header-home.component.svelte';
 </script>
 
+<!-- header -->
+<HeaderHome />
+
+<!-- main -->
 <Main>
   <slot />
 </Main>
