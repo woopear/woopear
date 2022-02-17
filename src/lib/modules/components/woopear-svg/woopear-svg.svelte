870c046399203ec04538fd0ab92043356f9092fa
j314h <johnhaimez314@gmail.com>
2022-02-17 18:23:24 +0100

    correction header + ajout logo woopear

---
+++ src/lib/modules/components/woopear-svg/woopear-svg.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  export let size = 'w-12 h-12';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" class={`${size}`} viewBox="0 0 1396 1396" fill="none">
+  <circle cx="698" cy="698" r="698" fill="white" />
+  <path
+    d="M670 299.104L776.481 692.909"
+    stroke="#0090E1"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M776 691.104L882.481 1084.91"
+    stroke="#0090E1"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M1096.48 302.104L989.999 695.909"
+    stroke="#AD00FF"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M990.48 694.104L883.999 1087.91"
+    stroke="#AD00FF"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M300 338.285L415.624 765.901"
+    stroke="#18AC00"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M420.608 781.779L503.592 1088.68"
+    stroke="#18AC00"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M723.643 488.303L614.38 786.859"
+    stroke="#FFE500"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+  <path
+    d="M612.876 791.492L503.613 1090.05"
+    stroke="#FFE500"
+    stroke-width="70"
+    stroke-linecap="round"
+  />
+</svg>
--- src/lib/modules/header/components/header-home.svelte
+++ src/lib/modules/header/components/header-home.svelte
@@ -1,10 +1,13 @@
 <script type="ts">
+  import WoopearSvg from '$lib/modules/components/woopear-svg/woopear-svg.svelte';
   import { themeModeStore } from '../../theme-mode/theme-mode.store';
 
   // boolean mode dark en fonction du themeMode
   $: btnDark = $themeModeStore === 'woo-claire' ? false : true;
 
-  // fonction pour switch de mode dark a mode light
+  /**
+   * fonction pour switch de mode dark a mode light
+   */
   const switchTheme = () => {
     // condition qui verifier dans quel théme on se trouve
     if ($themeModeStore === 'woo-claire') {
@@ -21,67 +24,58 @@
   };
 </script>
 
-<section>
-  <header class="h-16 bg-primary flex justify-between items-center px-6 md:px-44">
-    <!-- btn home -->
-    <div data-tip="Accueil" class="tooltip tooltip-bottom tooltip-secondary">
-      <button class="text-white">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-      </button>
-    </div>
+<header class="h-16 bg-primary flex justify-between items-center px-8 sm:px-24 md:px-36">
+  <!-- btn home -->
+  <section data-tip="Accueil" class="tooltip tooltip-bottom tooltip-secondary">
+    <a href="/" class="text-white">
+      <WoopearSvg size="w-10 h-10" />
+    </a>
+  </section>
 
-    <!-- switch dark mode -->
-    <div class="flex items-center">
-      <button class="text-white" on:click={switchTheme}>
-        {#if btnDark}
-          <li data-tip="mode claire" class="tooltip tooltip-bottom tooltip-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          </li>
-        {:else}
-          <li data-tip="mode foncé" class="tooltip tooltip-bottom tooltip-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </svg>
-          </li>
-        {/if}
-      </button>
-    </div>
-  </header>
-</section>
+  <!-- switch dark mode -->
+  <section class="flex items-center">
+    <button class="text-white flex items-center" on:click={switchTheme}>
+      {#if btnDark}
+        <li data-tip="mode claire" class="tooltip tooltip-bottom tooltip-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        </li>
+      {:else}
+        <li data-tip="mode foncé" class="tooltip tooltip-bottom tooltip-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </svg>
+        </li>
+      {/if}
+    </button>
+  </section>
+
+  <!-- TODO : pour le logo connexion
+    faire que quand il est connecter il aura une image de profil
+    et quand il est pas connecter il y aura une image de profil par defaut
+    cette image sera deja dans le stockage firebase -->
+  <!-- nav -->
+</header>
