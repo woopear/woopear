9ae00d2d78d4b8ca96d3279c000d05fcd787e781
David-Caignaert <davidcaignaert@gmail.com>
2022-02-18 16:13:57 +0100

    feat #120,ajout du component input

---
+++ src/lib/modules/components/input/input.component.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let span_text: string = '';
+  export let input_text: string = '';
+</script>
+
+<section class="form-control">
+  <span>{span_text}</span>
+  <input type="text" class="input input-bordered" value={input_text} />
+</section>
