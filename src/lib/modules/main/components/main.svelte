94549d2bb65220a3ad7300a87454c8e6584a2146
j314h <johnhaimez314@gmail.com>
2022-02-18 09:35:37 +0100

    profil user version 1

--- src/lib/modules/main/components/main.svelte
+++ src/lib/modules/main/components/main.svelte
@@ -1,3 +1,3 @@
-<main class="flex flex-col flex-1 justify-center items-center p-6 m-16">
+<main class="flex flex-col flex-1 justify-center items-center p-6 mt-16">
   <slot />
 </main>
--- src/lib/modules/user/components/user-profil.svelte
+++ src/lib/modules/user/components/user-profil.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import { session } from '$app/stores';
+
   import UserSvg from '$lib/modules/components/user-svg/user-svg.svelte';
   import { firstToUppperCase } from '$lib/providers/format/format.service';
   import { current_user_store } from '../user.store';
 </script>
 
 {#if $current_user_store && $current_user_store.uid}
-  <section class="my-12 card py-4 px-4 md:px-12 bg-base-200">
+  <section
+    class="my-12 card py-4 px-4 md:px-12 bg-base-200 w-full sm:w-9/12 md:w-6/12 lg:w-4/12 shadow-lg"
+  >
     <!-- image profil -->
     <section class="flex justify-center">
       {#if $current_user_store.avatar}
@@ -23,8 +27,22 @@
     <!-- information -->
     <section class="mt-8">
       <section>
-        <p>Nom : <strong>{$current_user_store.last_name}</strong></p>
-        <p>Prénom : <strong>{$current_user_store.first_name}</strong></p>
+        <!-- cadre adresse -->
+        <div>
+          <p class="text-secondary font-semibold mb-2 text-xl">Adresse</p>
+          <div class="pl-2 text-lg font-light">
+            <p>{$current_user_store.address}</p>
+            <p>{$current_user_store.code_post} {firstToUppperCase($current_user_store.city)}</p>
+          </div>
+        </div>
+        <!-- cadre info divers -->
+        <hr class="my-4" />
+        <div>
+          <p class="text-secondary font-semibold mb-2 text-xl">Infos</p>
+          <p>{$current_user_store.phone_number}</p>
+          <p>{$session.user.email}</p>
+          <p>mot de passe ******</p>
+        </div>
       </section>
     </section>
     <!-- lien utils changement d'email, changement mot de passe -->
