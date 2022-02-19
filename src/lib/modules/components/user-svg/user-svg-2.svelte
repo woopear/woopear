641eee79155afc7bd74e008008143fb44cd5feb1
j314h <johnhaimez314@gmail.com>
2022-02-19 18:12:42 +0100

    ajout tooltip custom

---
+++ src/lib/modules/components/user-svg/user-svg-2.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let size = 'h-5 w-5';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" class={`${size}`} viewBox="0 0 20 20" fill="currentColor">
+  <path
+    fill-rule="evenodd"
+    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+    clip-rule="evenodd"
+  />
+</svg>
--- src/lib/modules/menu-dashboard/components/menu-dashboard.svelte
+++ src/lib/modules/menu-dashboard/components/menu-dashboard.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import HomeSvg from '$lib/modules/components/home-svg/home-svg.svelte';
+  import TooltipCustom from '$lib/modules/components/tooltip/tooltip-custom.svelte';
+  import UserSvg_2 from '$lib/modules/components/user-svg/user-svg-2.svelte';
   import Logout from '$lib/modules/logout/components/logout.svelte';
+  import { ERole } from '$lib/modules/role/role.type';
+  import { current_user_store } from '$lib/modules/user/user.store';
 </script>
 
 <nav class="fixed navbar items-start mt-16 p-0 bg-base-200 top-0 bottom-0 left-0 z-20 w-14">
@@ -9,7 +13,7 @@
       <a href="/login">
         <Logout />
       </a>
-      <!--<TooltipCustom text="déconnection" />-->
+      <TooltipCustom text="déconnection" />
     </li>
     <li class="relative group">
       <a href="/dashboard">
@@ -17,6 +21,14 @@
       </a>
       <!--<TooltipCustom text="home" />-->
     </li>
+    {#if $current_user_store.role.libelle === ERole.ADMIN}
+      <li class="relative group">
+        <a href="/dashboard">
+          <UserSvg_2 size="w-7 h-7" />
+        </a>
+        <TooltipCustom text="utilisateurs" />
+      </li>
+    {/if}
   </ul>
 </nav>
 
