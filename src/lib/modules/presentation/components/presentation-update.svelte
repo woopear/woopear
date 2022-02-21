357bb100f6565a0c06e9dee148e779a53471c4c4
j314h <johnhaimez314@gmail.com>
2022-02-21 10:27:18 +0100

    feat #129 ; ajout textarea

--- src/lib/modules/presentation/components/presentation-update.svelte
+++ src/lib/modules/presentation/components/presentation-update.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import BtnCloseUpdate from '$lib/modules/components/btn/btn-close-update.svelte';
   import Input from '$lib/modules/components/input/input.svelte';
+  import Texarea from '$lib/modules/components/textarea/texarea.svelte';
   import Title from '$lib/modules/components/title/title.svelte';
   import { ETypeTitle } from '$lib/modules/components/title/title.type';
   import type { IPresentation } from '../presentation.type';
@@ -21,6 +22,7 @@
 
   <!-- corps -->
   <section class="mt-8">
+    <!-- form info principal -->
     <form>
       <!-- title -->
       <div>
@@ -32,11 +34,28 @@
         <label>Sous-titre de la présentation : </label>
         <Input name="sub_title" placeholder="sous titre" value={presentation_selected.sub_title} />
       </div>
+
       <!-- WOOPEAR -->
+      <span class=" block mt-12 mb-4">
+        <Title title="Partie entreprise" type_title={ETypeTitle.H6} />
+      </span>
       <!-- email -->
       <div>
         <label>E-mail de l'entreprise : </label>
-        <Input name="email" placeholder="email" value={presentation_selected.woopear.email} />
+        <Input
+          name="email"
+          placeholder="E-mail de l'entreprise"
+          value={presentation_selected.woopear.email}
+        />
+      </div>
+      <!-- phone_number -->
+      <div>
+        <label>Numero de téléphone : </label>
+        <Input
+          name="phone_number"
+          placeholder="Numero de téléphone"
+          value={presentation_selected.woopear.phone_number}
+        />
       </div>
       <!-- name -->
       <div>
@@ -47,11 +66,50 @@
           value={presentation_selected.woopear.name}
         />
       </div>
-
+      <!-- address -->
+      <div>
+        <label>L'addresse de l'entreprise : </label>
+        <Input
+          name="address"
+          placeholder="L'addresse de l'entreprise"
+          value={presentation_selected.woopear.address}
+        />
+      </div>
+      <!-- code_post -->
+      <div>
+        <label>Code postal de l'entreprise : </label>
+        <Input
+          name="code_post"
+          placeholder="Code postal"
+          value={presentation_selected.woopear.code_post}
+        />
+      </div>
+      <!-- city -->
+      <div>
+        <label>Ville de l'entreprise : </label>
+        <Input
+          name="city"
+          placeholder="Ville de l'entreprise"
+          value={presentation_selected.woopear.city}
+        />
+      </div>
+      <!-- map -->
+      <div>
+        <label>Coordonnées : </label>
+        <Input name="map" placeholder="Coordonnée" value={presentation_selected.woopear.map} />
+      </div>
+      <!-- info -->
+      <div>
+        <label>Info suplémentaire : </label>
+        <Texarea name="info" value={presentation_selected.woopear.info} />
+      </div>
+      <!-- btn modifier info pincipal -->
       <div class="flex justify-end">
         <button class="btn btn-primary mt-12">Modifier</button>
       </div>
     </form>
+
+    <!-- form content -->
   </section>
 </section>
 
