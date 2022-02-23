87f72f0976b2d4daa32648895e72282d79bc69fd
David-Caignaert <davidcaignaert@gmail.com>
2022-02-23 15:20:01 +0100

    feat #124;ajout store notification d un user par son id

--- src/lib/modules/notification/store/notification.store.ts
+++ src/lib/modules/notification/store/notification.store.ts
@@ -1,9 +1,10 @@
 import type { INotification } from './../notification.type';
 
 import { writable } from 'svelte/store';
-import { query, collection } from 'firebase/firestore';
+import { query, collection, where, onSnapshot, QuerySnapshot } from 'firebase/firestore';
 import { fire_db } from '$lib/providers/firebase/firebase.service';
 import { current_user_store } from '$lib/modules/user/user.store';
+import type { Unsubscribe } from 'firebase/auth';
 
 
 /* 
@@ -11,6 +12,9 @@ import { current_user_store } from '$lib/modules/user/user.store';
 */
 const createNotificationStore = () => {
   const { set, update, subscribe } = writable([] as INotification[])
+
+  // ecouteur du document notification du user connecté
+  let lisen_get_notification: Unsubscribe;
 
   return {
     set,
@@ -20,13 +24,20 @@ const createNotificationStore = () => {
     /* 
     * 
     */
-    async listenNotification(uid: string): Promise<void> {
+    async listenNotificationUser(uid: string): Promise<void> {
       let notifications: INotification[] = [];
-
+      
       // query pour récupèrer la collection notification sur firebase
-      const notification_query = query(collection(fire_db, 'notifications', `uid: ${uid}`))
-      
-      
+      const notification_query = query(collection(fire_db, 'notifications'), where ('uid', '==', `${uid}`));
+      lisen_get_notification = onSnapshot(notification_query, (QuerySnapshot)=> {
+        let notification:INotification;
+        QuerySnapshot.forEach((doc) => {
+          notification = { id: doc.id, ...doc.data() }
+          notifications =[...notifications, notification]
+          set(notifications);
+        })
+      })
+
     }
   }
 }
