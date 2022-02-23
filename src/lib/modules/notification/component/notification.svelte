12ab0e7054483fd31db0c61fb7be4a66e16e1017
David-Caignaert <davidcaignaert@gmail.com>
2022-02-23 15:50:37 +0100

    feat #124;dans le composent ajout de la boucle sur le store_notification

--- src/lib/modules/notification/component/notification.svelte
+++ src/lib/modules/notification/component/notification.svelte
@@ -2,73 +2,34 @@
   import { store_notification } from '../store/notification.store';
 </script>
 
-<section class=" rounded-lg fixed top-20 right-8 w-full sm:w-5/12 md:w-4/12 z-50">
-  <section class=" alert shadow-lg bg-opacity-100 alert-error text-white">
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="stroke-current flex-shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      <span>Error! Task failed successfully.</span>
-    </div>
+{#if $store_notification.length > 0}
+  <section class=" rounded-lg fixed top-20 right-8 w-full sm:w-5/12 md:w-4/12 z-50">
+    {#each $store_notification as notification}
+      {#if notification.type === 'warning'}
+        <section class="mt-2 alert shadow-lg bg-opacity-100 alert-warning text-white">
+          <div>
+            <span>{notification.libelle}</span>
+          </div>
+        </section>
+      {:else if notification.type === 'error'}
+        <section class="mt-2 alert shadow-lg bg-opacity-100 alert-error text-white">
+          <div>
+            <span>{notification.libelle}</span>
+          </div>
+        </section>
+      {:else if notification.type === 'success'}
+        <section class="mt-2 alert shadow-lg bg-opacity-100 alert-success text-white">
+          <div>
+            <span>{notification.libelle}</span>
+          </div>
+        </section>
+      {:else if notification.type === 'info'}
+        <section class="mt-2 alert shadow-lg bg-opacity-100 alert-info text-white">
+          <div>
+            <span>{notification.libelle}</span>
+          </div>
+        </section>
+      {/if}
+    {/each}
   </section>
-  <section class=" alert shadow-lg bg-opacity-100 alert-warning text-white">
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="stroke-current flex-shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      <span>Error! Task failed successfully.</span>
-    </div>
-  </section>
-  <section class=" alert shadow-lg bg-opacity-100 alert-success text-white">
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="stroke-current flex-shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      <span>Error! Task failed successfully.</span>
-    </div>
-  </section>
-  <section class=" alert shadow-lg bg-opacity-100 alert-info text-white">
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="stroke-current flex-shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      <span>Error! Task failed successfully.</span>
-    </div>
-  </section>
-</section>
+{/if}
