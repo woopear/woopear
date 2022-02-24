b2c72f106acfdb6274fd8773f2edcb2641166a65
David-Caignaert <davidcaignaert@gmail.com>
2022-02-24 14:12:51 +0100

    feat #124;dans le component ajout du boutton supprimer notification

--- src/lib/modules/notification/component/notification.svelte
+++ src/lib/modules/notification/component/notification.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <!-- si le tableau de notification n'est pas vide -->
-{#if $store_notification.length > 0}
+{#if $store_notification?.length > 0}
   <section class=" rounded-lg fixed top-20 right-8 w-full sm:w-5/12 md:w-4/12 z-50">
     <!-- boucle sur le tableau notification -->
     {#each $store_notification as notification}
@@ -23,12 +23,56 @@
           <div>
             <span>{notification.libelle}</span>
           </div>
+          <!-- boutton supprimer notification -->
+          <div class="ml-auto mb-4">
+            <button
+              class="btn btn-circle btn-sm sm:btn-md text-primary bg-slate-100 border-none hover:bg-slate-300 "
+              on:click={() => store_notification.removeNotificationUser(notification.id)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </section>
         <!-- si la notification est de type error -->
       {:else if notification.type === 'error'}
         <section class="mt-2 alert shadow-lg bg-opacity-100 alert-error text-white">
           <div>
             <span>{notification.libelle}</span>
+          </div>
+          <!-- boutton supprimer notification -->
+          <div class="ml-auto mb-4">
+            <button
+              class="btn btn-circle btn-sm sm:btn-md text-primary bg-slate-100 border-none hover:bg-slate-300 "
+              on:click={() => store_notification.removeNotificationUser(notification.id)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
           </div>
         </section>
         <!-- si la notification est de type success -->
@@ -37,12 +81,56 @@
           <div>
             <span>{notification.libelle}</span>
           </div>
+          <!-- boutton supprimer notification -->
+          <div class="ml-auto mb-4">
+            <button
+              on:click={() => store_notification.removeNotificationUser(notification.id)}
+              class="btn btn-circle btn-sm sm:btn-md text-primary bg-slate-100 border-none hover:bg-slate-300 "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </section>
         <!-- si la notification est de type info -->
       {:else if notification.type === 'info'}
         <section class="mt-2 alert shadow-lg bg-opacity-100 alert-info text-white">
           <div>
             <span>{notification.libelle}</span>
+          </div>
+          <!-- boutton supprimer notification -->
+          <div class="ml-auto mb-4">
+            <button
+              on:click={() => store_notification.removeNotificationUser(notification.id)}
+              class="btn btn-circle btn-sm sm:btn-md text-primary bg-slate-100 border-none hover:bg-slate-300 "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
           </div>
         </section>
       {/if}
