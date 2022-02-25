3d2e1b0e6165a929d27d50c310243a54027dba9f
David-Caignaert <davidcaignaert@gmail.com>
2022-02-25 12:00:58 +0100

    feat #127; creation de l ecouteur advertising

---
+++ src/lib/modules/advertising/store/advertising.store.ts
@@ -0,0 +1,39 @@
+import { onSnapshot, collection, doc, serverTimestamp } from 'firebase/firestore';
+import type { IAdverting } from './../advertising.type';
+import { writable } from 'svelte/store';
+import type { Unsubscribe } from 'firebase/auth';
+import { fire_db } from '$lib/providers/firebase/firebase.service';
+
+// création du store advertising
+function createStoreAdvertising() {
+  const { set, update, subscribe } = writable([] as IAdverting[]);
+
+  // ecouteur de advertising
+  let listen_advertising: Unsubscribe;
+
+  return {
+    subscribe,
+    set,
+    update,
+    listen_advertising,
+
+    /* 
+     *ecoute de advertising
+     */
+    listenAdvertising: function (): void {
+      // ecouteur
+      listen_advertising = onSnapshot(collection(fire_db, 'advertisings'), (snapShot) => {
+        let advertisings: IAdverting[] = [];
+        // on parcours les documents
+        snapShot.forEach((doc) => {
+          // creation du document
+          const obj_advertising = { id: doc.id, ...doc.data()};
+          // ajout dans le tableau de advertising
+          advertisings = [...advertisings, obj_advertising];
+        });
+        // on set le store
+        set(advertisings);
+      });
+    }
+  }
+}
