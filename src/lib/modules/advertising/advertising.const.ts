4b8d169ec50d1b58ed07dab0242e727d49cbac87
David-Caignaert <davidcaignaert@gmail.com>
2022-03-01 11:06:25 +0100

    feat #127; ajouter des 2 fonction dans fichier type pour créer un objet advertising et advertisinf-article

--- src/lib/modules/advertising/advertising.const.ts
+++ src/lib/modules/advertising/advertising.const.ts
@@ -1,20 +1,28 @@
-import type { IAdverting } from './advertising.type';
+import type { IAdverting, IAdvertingArticle } from './advertising.type';
+
 // notification pour la publicité
 export const enum EAdvertisingNotif {
   ADD_SUCCES = 'votre publicité a bien été créé',
   ADD_ERROR = 'impossible de créer votre publicité, une erreur est survenue',
 
-  UPDATE_SUCCES = '',
-  UPDATE_ERROR = '',
+  UPDATE_SUCCES = 'votre publicité a bien été modifié',
+  UPDATE_ERROR = 'impossible de modifier votre publicité, une erreur est survenue',
 
   DELETE_SUCCES = 'votre publicité a bien été supprimé',
   DELETE_ERROR = 'impossible de supprimer votre publicité, une erreur est survenue',
 }
 
-
 export const enum EAdvertisingSelectedNotif {
-  ADD_SUCCES = '',
-  ADD_ERROR = '',
+  UPDATE_SUCCES = '',
+  UPDATE_ERROR = '',
+
+  DELETE_SUCCES = '',
+  DELETE_ERROR = '',
+}
+
+export const enum EAdvertisingArticleSelectedNotif {
+  ADD_SUCCES = 'votre article de publicité a bien été créé',
+  ADD_ERROR = 'impossible de créer votre article de publicité, une erreur est survenue',
 
   UPDATE_SUCCES = '',
   UPDATE_ERROR = '',
@@ -31,6 +39,15 @@ export function createAdvertisingObject(): IAdverting {
             description: '',
             libelle: ''
           },
+          title:''
+  }
+}
+
+export function createAdvertisingArticleObject(): IAdvertingArticle {
+  return {
+    active: false,
+          image: '',
+          content: [],
           title:''
   }
 }
