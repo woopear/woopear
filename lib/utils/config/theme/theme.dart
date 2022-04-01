fba7c888b65fa4bf03dd8c0c678f9fb30c4f2036
john_dev <johnhaimez314@gmail.com>
2022-04-01 19:11:53 +0200

    ajout couleur

--- lib/utils/config/theme/theme.dart
+++ lib/utils/config/theme/theme.dart
@@ -1,9 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:woo_theme_mode/woo_theme_mode.dart';
 
 /// theme mode claire
-final themeClaire = WooTheme.modeClair();
+final themeClaire = WooTheme.modeClair(
+  scaffolBackground: Colors.white,
+  primary: const Color(0xFF0090E1),
+  secondary: const Color(0xFFAD00FF),
+  tertiary: const Color(0xFFE3CC00),
+);
 
 /// theme mode dark
-final themeDark = WooTheme.modeDark();
+final themeDark = WooTheme.modeDark(
+  scaffolBackground: const Color(0xFF36393F),
+  primary: const Color(0xFF40BAFF),
+  secondary: const Color(0xFFC854FF),
+  tertiary: const Color(0xFFFFE500),
+);
 
 
