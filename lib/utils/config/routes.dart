3ba4b9ca3e80ba2677e765bcc29ec0bc8f1ec991
john_dev <johnhaimez314@gmail.com>
2022-04-01 18:23:25 +0200

    ajout page john

--- lib/utils/config/routes.dart
+++ lib/utils/config/routes.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:woopear/pages/home.dart';
+import 'package:woopear/pages/john.dart';
 
 class Routes {
   /// les routes
   final String _home = '/';
+  final String _john = '/john';
 
   /// les getters des routes
   String get home => _home;
+  String get john => _john;
 
   /// retourne un widget en fonction de la routes
   Map<String, Widget Function(BuildContext)> urls() {
     return {
       _home: (context) => const Home(),
+      _john: (context) => const John(),
     };
   }
 }
