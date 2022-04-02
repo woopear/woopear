8b4da140b4dac2cf1fd7b0d7819136e016439667
David-Caignaert <davidcaignaert@gmail.com>
2022-04-02 10:37:43 +0200

    creation du user_const

---
+++ lib/models/user/user_const.dart
@@ -0,0 +1,27 @@
+class UserConst {
+  /// connexion user
+  static String titlePageConnexion = "Connexion Flutoo";
+  static String connexionUserEmailMessageError = "l'email n'existe pas";
+  static String connexionUserPasswordError = "le mot de passe n'est pas bon";
+  static String connexionSucces = "Connexion réussis";
+  static String connexionError = "Erreur de connexion";
+  static String labelInputEmail = "Votre identifiant";
+  static String labelInputPassword = "Votre mot de passe";
+  static String btnConnexion = "Se connecter";
+
+  /// inscription user
+  static String inscriptionUserMessageSucces = 'Inscription reussi';
+  static String inscriptionUserEmailError = "L'email existe déjà";
+  static String inscriptionUserPasswordError =
+      'Le mot de passe fourni est trop faible';
+  static String? inscriptionError = "Erreur de connexion";
+  static String? labelCreateInputEmail = "Votre adresse email *";
+  static String? labelCreateInputPassword = "Votre mot de passe *";
+  static String? labelInputPseudo = "Votre pseudo";
+  static String? labelInputLastName = "Votre nom";
+  static String? labelInputFirstName = "Votre prénom";
+  static String? btnCreateUser = "S'inscrire";
+  static String? titlePageCreate = "Créer mon compte";
+  static String? infoFormCreate = "* Ce champs est obligatoire";
+  
+}
