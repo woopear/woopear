9447f43da7592143cbf472b1c929112f3326f6f2
john_dev <johnhaimez314@gmail.com>
2022-04-07 09:29:23 +0200

    init presentation list

---
+++ lib/models/presentation/widgets/presentation_list/presentation_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PresentationList extends ConsumerStatefulWidget {
+  const PresentationList({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _PresentationListState();
+}
+
+class _PresentationListState extends ConsumerState<PresentationList> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
