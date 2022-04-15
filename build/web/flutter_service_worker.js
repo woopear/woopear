c89e7c7a67bf8d9e5c94e934ad6b52b75c080e73
john_dev <johnhaimez314@gmail.com>
2022-04-15 10:30:36 +0200

    build web version 0.0.3

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e7b458f6fbed554c0aadfa9c92e9f3ff",
-"/": "e7b458f6fbed554c0aadfa9c92e9f3ff",
-"main.dart.js": "b47f622016f0a4dd09e8c55d670514c2",
+"index.html": "4e7609393abfa7d5d8c5d1a0a44a6ba3",
+"/": "4e7609393abfa7d5d8c5d1a0a44a6ba3",
+"main.dart.js": "81157cb02e7f2e9495e3237e44fd4c4e",
 "manifest.json": "c6500aa7d2b1046061caf62cc781689e",
 "version.json": "ce4c38626f58509b80770e4d3cee6601"
 };
