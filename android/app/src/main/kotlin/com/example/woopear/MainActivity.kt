c03d370a0b5caa658bf2b87b15c9267a5706819b
j314h <johnhaimez314@gmail.com>
2022-06-19 15:17:39 +0200

    delete v1

--- android/app/src/main/kotlin/com/example/woopear/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.woopear
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/config/theme/icon_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const iconThemeDark = IconThemeData(
-  color: Color(0xFF40BAFF),
-);
-
-const iconThemeClaire = IconThemeData(
-  color: Color(0xFF0090E1),
-);
